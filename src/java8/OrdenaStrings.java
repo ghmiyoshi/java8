34c5667eda274dd2640a9e143fb5502a41459748
Gabriel Hideki <gabriel_hidekii@hotmail.com>
2019-04-19 00:35:12 -0300

    Lambdas

--- src/java8/OrdenaStrings.java
+++ src/java8/OrdenaStrings.java
@@ -3,6 +3,7 @@ package java8;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class OrdenaStrings {
 
@@ -13,11 +14,20 @@ public class OrdenaStrings {
 		palavras.add("editora casa do código");
 		palavras.add("caelum");
 
-		ComparatorLenght comparador = new ComparatorLenght();
-		
+		// ComparatorLenght comparador = new ComparatorLenght();
+		// Consumidor consumidor = new Consumidor();
+
 		// Collections.sort(palavras, comparador);
-	
-		palavras.sort(comparador);
+
+		palavras.sort((s1,s2) ->{
+			if (s1.length() < s2.length())
+				return -1;
+			if (s1.length() > s2.length())
+				return 1;
+			return 0;			
+		});
+		
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
 
 //		for(String palavra : palavras) {
 //			System.out.println(palavra);
@@ -29,15 +39,25 @@ public class OrdenaStrings {
 
 }
 
-class ComparatorLenght implements Comparator<String> {
-
-	@Override
-	public int compare(String s1, String s2) {
-		if (s1.length() < s2.length())
-			return -1;
-		if (s1.length() > s2.length())
-			return 1;
-		return 0;
-	}
-
-}
+//class ComparatorLenght implements Comparator<String> {
+//
+//	@Override
+//	public int compare(String s1, String s2) {
+//		if (s1.length() < s2.length())
+//			return -1;
+//		if (s1.length() > s2.length())
+//			return 1;
+//		return 0;
+//	}
+//
+//}
+//
+//class Consumidor implements Consumer<String> {
+//
+//	@Override
+//	public void accept(String s) {
+//		System.out.println(s);
+//
+//	}
+//
+//}
