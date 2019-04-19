23ad65d1d78e0580312713aeac453aeee746d614
Gabriel Hideki <gabriel_hidekii@hotmail.com>
2019-04-19 15:33:49 -0300

    Ordenação antiga

--- src/java8/OrdenaStrings.java
+++ src/java8/OrdenaStrings.java
@@ -1,6 +1,7 @@
 package java8;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
@@ -14,50 +15,47 @@ public class OrdenaStrings {
 		palavras.add("editora casa do código");
 		palavras.add("caelum");
 
-		// ComparatorLenght comparador = new ComparatorLenght();
-		// Consumidor consumidor = new Consumidor();
+		System.out.println("*** Lista de palavras ***");
+		Consumidor consumidor = new Consumidor();
+		palavras.forEach(consumidor);
 
-		// Collections.sort(palavras, comparador);
-
-		palavras.sort((s1,s2) ->{
-			if (s1.length() < s2.length())
-				return -1;
-			if (s1.length() > s2.length())
-				return 1;
-			return 0;			
-		});
-		
-		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
-
-//		for(String palavra : palavras) {
+//		for (String palavra : palavras) {
 //			System.out.println(palavra);
 //		}
 
-		palavras.forEach(palavra -> System.out.println(palavra));
+		System.out.println("\n*** Lista ordenada pelo tamanho dos caracteres ***");
+		ComparatorLenght comparador = new ComparatorLenght();
+		palavras.sort(comparador);
+
+		for (String palavra : palavras) {
+			System.out.println(palavra);
+		}
+
+		// Collections.sort(palavras, comparador);
 
 	}
 
 }
 
-//class ComparatorLenght implements Comparator<String> {
-//
-//	@Override
-//	public int compare(String s1, String s2) {
-//		if (s1.length() < s2.length())
-//			return -1;
-//		if (s1.length() > s2.length())
-//			return 1;
-//		return 0;
-//	}
-//
-//}
-//
-//class Consumidor implements Consumer<String> {
-//
-//	@Override
-//	public void accept(String s) {
-//		System.out.println(s);
-//
-//	}
-//
-//}
+class ComparatorLenght implements Comparator<String> {
+
+	@Override
+	public int compare(String s1, String s2) {
+		if (s1.length() < s2.length())
+			return -1;
+		if (s1.length() > s2.length())
+			return 1;
+		return 0;
+	}
+
+}
+
+class Consumidor implements Consumer<String> {
+
+	@Override
+	public void accept(String s) {
+		System.out.println(s);
+
+	}
+
+}
